7dea677af241bcd9de7bffdb97afcda759620770
Antonio Caria <92182190+Ant8off@users.noreply.github.com>
2023-01-24 16:35:29 +0100

    added main test keyboard interaction

--- src/reversepolish/Reversepolish.java
+++ src/reversepolish/Reversepolish.java
@@ -1,9 +1,13 @@
 package reversepolish;
 
+import java.util.Scanner;
+
 public class Reversepolish {
 
+    public static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         
+        /*
         expression e1 = new expression("((10 * 2) + (4 - 5)) / 2");
         
         
@@ -16,6 +20,28 @@ public class Reversepolish {
         expression e3 = new expression("5 + (10 * 2)");
         
         System.out.println(e3.toString());
+        */
+        
+        String input;
+        
+        System.out.println("+-------------------------------------------------------------------------+");
+        System.out.println("  _____  _____  _   _    _____                          _            \n" +
+                           " |  __ \\|  __ \\| \\ | |  / ____|                        | |           \n" +
+                           " | |__) | |__) |  \\| | | |     ___  _ ____   _____ _ __| |_ ___ _ __ \n" +
+                           " |  _  /|  ___/| . ` | | |    / _ \\| '_ \\ \\ / / _ \\ '__| __/ _ \\ '__|\n" +
+                           " | | \\ \\| |    | |\\  | | |___| (_) | | | \\ V /  __/ |  | ||  __/ |   \n" +
+                           " |_|  \\_\\_|    |_| \\_|  \\_____\\___/|_| |_|\\_/ \\___|_|   \\__\\___|_|   ");
+        
+        System.out.println("\n\n\n");
+        System.out.print("Input your expression: ");
+        input = sc.nextLine();
+        
+        expression e1 = new expression(input);
+        
+        System.out.println("RPN CONVERTED -> " + e1.toString());
+        
+        System.out.println("+-------------------------------------------------------------------------+");
+        
         
     }
     
